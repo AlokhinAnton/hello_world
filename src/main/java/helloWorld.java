3e8aaa4a6a9495cb8dc7cceaaf96fa31bea36b1a
Anton <SomePassw0rd>
2021-05-19 16:45:53 +0300

    added new line

--- src/main/java/helloWorld.java
+++ src/main/java/helloWorld.java
@@ -4,5 +4,7 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello agian");
+        int a = 1;
+        System.out.printf("a");
     }
 }
